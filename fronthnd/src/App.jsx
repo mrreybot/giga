6c71b840e7cc58d4e558d0f5591cd0e10759ffc4
Reyhan Imece <reyhanimece@Reyhan-MacBook-Pro.local>
2025-10-28 12:30:10 +0300

    frnt duzen

--- fronthnd/src/App.jsx
+++ fronthnd/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/Dashoard";
+import Dashboard from "./pages/DashBoard";
 
 function App() {
   return (
