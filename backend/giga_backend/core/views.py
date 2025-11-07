679970b5f1def6c3d170df9da0bba05aed916fd1
Selimhaan <saltinpinar8@gmail.com>
2025-11-07 14:41:03 +0300

    backend task endpointleri

--- backend/giga_backend/core/views.py
+++ backend/giga_backend/core/views.py
@@ -9,3 +9,42 @@ class CreateUserView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]
+
+
+
+
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Mission
+from .serializers import MissionSerializer
+
+@api_view(['GET', 'POST'])
+def task_list(request):
+    if request.method == 'GET':
+        tasks = Mission.objects.all().order_by('-id')
+        serializer = MissionSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MissionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PATCH'])
+def task_detail(request, pk):
+    try:
+        task = Mission.objects.get(pk=pk)
+    except Mission.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = MissionSerializer(task, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
