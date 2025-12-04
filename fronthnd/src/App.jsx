1da73e5f741f1b69ad01cea119fb61beb3ad73c9
Reyhan Imece <reyhanimece@Reyhan-MacBook-Pro.local>
2025-12-04 15:15:26 +0300

    Projenin son hali

--- fronthnd/src/App.jsx
+++ fronthnd/src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/DashBoard";
+import LoginPage from "./Pages/LoginPage";
+import HomePage from "./Pages/HomePage";
+import Dashboard from "./Pages/DashBoard";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Login sayfası - Sidebar YOK */}
         <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        
+        {/* Diğer sayfalar - Sidebar VAR */}
+        <Route path="/home" element={<Layout><HomePage /></Layout>} />
+        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+        <Route path="/settings" element={<Layout><div style={{padding: '2rem'}}>⚙️ Ayarlar sayfası yakında...</div></Layout>} />
       </Routes>
     </Router>
   );
