6abb6c52d9a8e9e1b786de5c82c0a5556bca463f
Reyhan Imece <reyhanimece@Reyhan-MacBook-Pro.local>
2025-12-04 16:04:06 +0300

    Stat eklendi

--- fronthnd/src/App.jsx
+++ fronthnd/src/App.jsx
@@ -3,7 +3,7 @@ import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import Dashboard from "./Pages/DashBoard";
 import Layout from "./components/Layout";
-
+import Statistics from "./Pages/Statistics"
 function App() {
   return (
     <Router>
@@ -14,7 +14,9 @@ function App() {
         {/* Diğer sayfalar - Sidebar VAR */}
         <Route path="/home" element={<Layout><HomePage /></Layout>} />
         <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+        <Route path="/statistics" element={<Layout><Statistics /></Layout>} />
         <Route path="/settings" element={<Layout><div style={{padding: '2rem'}}>⚙️ Ayarlar sayfası yakında...</div></Layout>} />
+       
       </Routes>
     </Router>
   );
