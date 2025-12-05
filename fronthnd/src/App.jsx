6bc2f29099cdb7648e87b7e5902e684aa507f10c
Selimhaan <saltinpinar8@gmail.com>
2025-12-05 16:41:38 +0300

    mainde

--- fronthnd/src/App.jsx
+++ fronthnd/src/App.jsx
@@ -6,23 +6,30 @@ import Layout from "./components/Layout";
 import Statistics from "./Pages/Statistics";
 import Archive from "./Pages/ArchivePage";
 import AddTask from "./Pages/AddTask";
+import './styles/theme-variables.css';
+import { ThemeProvider } from './contexts/ThemeContext';
+import './styles/dark-theme.css';
+import './styles/ThemeToggle.css';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Login sayfası - Sidebar YOK */}
-        <Route path="/" element={<LoginPage />} />
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          {/* Login sayfası - Sidebar YOK */}
+          <Route path="/" element={<LoginPage />} />
+          
+          {/* Diğer sayfalar - Sidebar VAR */}
+          <Route path="/arsiv" element={<Layout><Archive /></Layout>} />
+          <Route path="/add-task" element={<Layout><AddTask /></Layout>} />
+          <Route path="/home" element={<Layout><HomePage /></Layout>} />
+          <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+          <Route path="/statistics" element={<Layout><Statistics /></Layout>} />
+          <Route path="/settings" element={<Layout><div style={{padding: '2rem'}}>⚙️ Ayarlar sayfası yakında...</div></Layout>} />
         
-        {/* Diğer sayfalar - Sidebar VAR */}
-        <Route path="/arsiv" element={<Layout><Archive /></Layout>} />
-        <Route path="/add-task" element={<Layout><AddTask /></Layout>} />
-        <Route path="/home" element={<Layout><HomePage /></Layout>} />
-        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
-        <Route path="/statistics" element={<Layout><Statistics /></Layout>} />
-        <Route path="/settings" element={<Layout><div style={{padding: '2rem'}}>⚙️ Ayarlar sayfası yakında...</div></Layout>} />
-       
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
