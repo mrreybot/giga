7bc62943cd9c60867a08c8ad12fa57f206cf5d96
Buğra Keşli <omerbugraakesli@gmail.com>
2025-12-09 01:42:48 +0300

    Merge işlemi tamamlandı ve settings.py dosyası gün cellendi

--- backend/giga_backend/giga_backend/settings.py
+++ backend/giga_backend/giga_backend/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ============================================================
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://giga-gcju.onrender.com",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
@@ -166,6 +167,7 @@ CORS_ALLOW_HEADERS = [
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
+    'https://giga-gcju.onrender.com',
     'http://127.0.0.1:5173',
     'http://192.168.1.106:8081',
 ]
